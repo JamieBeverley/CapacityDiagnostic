52a60e600bcb1ecf47d6dde21fbf04ae660afb97
Jamie Beverley <jamie_beverley@hotmail.com>
2017-03-04 22:37:00 -0500

    bootstrap styling added

--- CapacityDiagnostic/settings.py
+++ CapacityDiagnostic/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'Patient.apps.PatientConfig',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
